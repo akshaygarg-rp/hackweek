6b1b3ad14c7a5701bc506c308fe0c6ffcb03f80e
Akshay Garg <akshaygarg@Akshays-MBP.attlocal.net>
2023-04-06 18:25:17 -0700

    added sticky header to the table

--- src/App.js
+++ src/App.js
@@ -7,28 +7,30 @@ import response from './data.json';
 function App() {
   return (
     <div className="App">
-      <table class="table table-bordered">
-          <thead>
-              <tr>
-                  <th>Model name</th>
-                  <th>Source class</th>
-                  <th>Type of permission(model/field)</th>
-                  <th>Field name</th>
-                  <th>Action type</th>
-                  <th>Source class</th>
-                  <th>Type</th>
-                  <th>Required name</th>
-                  <th>Required expr</th>
-              </tr>
-          </thead>
-          <tbody>
-          {Object.values(response.models).map((model) => {
-            return (
-                <ModelView model={model} />
-            );
-          })}
-          </tbody>
-      </table>
+      <div className='tableFixHead'>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Model name</th>
+                    <th>Source class</th>
+                    <th>Type of permission(model/field)</th>
+                    <th>Field name</th>
+                    <th>Action type</th>
+                    <th>Source class</th>
+                    <th>Type</th>
+                    <th>Required name</th>
+                    <th>Required expr</th>
+                </tr>
+            </thead>
+            <tbody>
+            {Object.values(response.models).map((model) => {
+              return (
+                  <ModelView model={model} />
+              );
+            })}
+            </tbody>
+        </table>
+      </div> 
     </div>
   );
 }
